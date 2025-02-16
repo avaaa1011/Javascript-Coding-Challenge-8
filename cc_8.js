531efb607b701d2285485a17818e0010c1acb247
Ava Tran <ava31@usf.edu>
2025-02-16 00:53:00 -0500

    Task 4: Car Rental Cost Calculation

--- cc_8.js
+++ cc_8.js
@@ -16,13 +16,23 @@ const calculateDiscount = function(price, discountRate) //declaring a function
 console.log(calculateDiscount(100, 0.2)); //test data that should produce: "Final Price: $80.00"
 console.log(calculateDiscount(250, 0.15)); //test data that should produce: "Final Price: $212.50"
 
-// Task 3: Arrow Function
-
+// Task 3: Service Fee Calculation 
 const calculateServiceFee = (amount, serviceType) => 
-    {
+    {//arrow function that will help determine a percentage fee based on the service type chosen 
         let feeRate = serviceType === "Premium" ? 0.15 : serviceType === "Standard" ? 0.10 : serviceType === "Basic" ? 0.05 : 0;
         let serviceFee = amount * feeRate;
         return `Service Fee: $${serviceFee.toFixed(2)}`;
     };
-console.log(calculateServiceFee(200, "Premium"));  // Expected output: "Service Fee: $30.00"
-console.log(calculateServiceFee(500, "Standard")); // Expected output: "Service Fee: $50.00"
+console.log(calculateServiceFee(200, "Premium"));  // test data that should produce:  "Service Fee: $30.00"
+console.log(calculateServiceFee(500, "Standard")); // test data that should produce: "Service Fee: $50.00"
+
+//Task 4: Car Rental Cost Calculation 
+function calculateRentalCost(days, carType, insurance = false)//function that calculates the rental costs and types
+    { //defining the different types of rental cars and its costs
+        let rate = carType === "Economy" ? 40 :carType === "Standard" ? 60 :carType === "Luxury" ? 100 : 0; 
+        let insuranceCost = insurance ? 20 * days : 0; //Extra days will costs $20 each day
+        let totalCost = (rate * days) + insuranceCost;
+        return `Total Rental Cost: $${totalCost}`;
+    }
+    console.log(calculateRentalCost(3, "Economy", true)); // test data that should produce: "Total Rental Cost: $180"
+    console.log(calculateRentalCost(5, "Luxury", false)); // test data that should produce: "Total Rental Cost: $500"
